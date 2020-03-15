56c72358ea9514f1aeea5991ae80310e1e2a04f1
KoTamaki <koukoukokoko@icloud.com>
2020-03-16 07:58:58 +0900

    メモアプリ作った

--- src/App.js
+++ src/App.js
@@ -1,113 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import Memo from './memo/Memo';
+import AddForm from './memo/AddForm';
+import FindForm from './memo/FindForm';
+import DelForm from './memo/DelForm';
 
-//ステートのマッピング
-function mappingState(state) {
-	return state;
-}
-
-//Appコンポーネントを作成する
-
+//Appコンポーネント
 class App extends Component {
+	td = {
+		width:'250px'
+	}
 
 	constructor(props){
 		super(props);
 	}
 
 	render(){
-		return (
+		return(
 			<div>
-				<Num />
-				<Red />
-				<Blue />
+				<h1>memo</h1>
+				<AddForm />
+				<hr />
+				<table><tbody><tr>
+					<td style={this.td}><FindForm /></td>
+					<td style={this.td}><DelForm /></td>
+				</tr></tbody></table>
+				<Memo />
 			</div>
-		)
+		);
 	}
 }
 
-//ストアのコネクト
-App = connect() (App);
-
-//メッセージ表示のコンポーネント
-class Num extends Component {
-	style = {
-		fontSize: '20pt',
-		padding: '20px 5px'
-	}
-
-	render(){
-		return (
-			<p style={this.style}>
-				{this.props.message}: {this.props.counter}
-			</p>
-		)
-	}
-}
-
-//ストアのコネクト
-Num = connect(mappingState) (Num);
-
-//ボタン表示のコンポーネント　
-class Red extends Component {
-	style = {
-		fontSize:'16pt',
-		padding:'5px 10px'
-	}
-
-	constructor(props){
-		super(props);
-		this.doAction = this.doAction.bind(this)
-	}
-
-	//ボタンクリックでディスパッチを実行
-	doAction(e){
-			this.props.dispatch({ type:'DOWN' })
-	}
-
-	render(){
-		return (
-			<button style={this.style}
-				onClick={this.doAction}>
-			あか	
-			</button>
-		)
-	}
-}
-
-//ストアのこね
-Red = connect() (Red);
-
-//ボタン表示のコンポーネント　
-class Blue extends Component {
-	style = {
-		fontSize:'16pt',
-		padding:'5px 10px'
-	}
-
-	constructor(props){
-		super(props);
-		this.doAction = this.doAction.bind(this)
-	}
-
-	//ボタンクリックでディスパッチを実行
-	doAction(e){
-			this.props.dispatch({ type:'UP' })
-	}
-
-	render(){
-		return (
-			<button style={this.style}
-				onClick={this.doAction}>
-				青	
-			</button>
-		)
-	}
-}
-
-//ストアのこね
-Blue = connect() (Blue);
-
-
-export default connect()(App);
-
+export default connect() (App);
--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom' 
-import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-
-//ステートとは初期値のことだ
-
-let init = {
-	counter:0,
-	message: '好きなボタンを押して！',
-}
-
-//レデューサーはメッセージによって別のアクションをさせる分岐のこと 
-function counter(state = init, action){
-	switch(action.type){
-		case 'UP':
-		return{
-			counter: state.counter + 1,
-			message:'増えた！'
-		};
-		case'DOWN':
-		return{
-			counter: state.counter - 1,
-			message:'減った！'
-		};
-		default:
-		return state;
-	}
-}
-
-
-//ストアを作
-let store = createStore(counter);
+import MemoStore from './memo/Store'
 
 //表示をレンダリング
 ReactDOM.render(
-	<Provider store={store}>
+	<Provider store={MemoStore}>
 		<App />
 	</Provider>,
 	document.getElementById('root')
 );
-
 
